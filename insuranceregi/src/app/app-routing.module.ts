10e4c569075d1d59cea730bbb7b2f3cccbf35a5b
gowlikardheeraj08 <gowlikardheeraj08@gmail.com>
2022-07-25 10:51:54 +0530

    home added

--- insuranceregi/src/app/app-routing.module.ts
+++ insuranceregi/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { BuyComponent } from './buy-insurance/buy-insurance.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',component:HomeComponent
+  },
+{
+  path:'homeLink',component:HomeComponent
+},
+{
+  path:'buyLink',component:BuyComponent
+},
+{
+  path:'adminLink',component:AdminComponent
+},
+{
+  path:'loginLink',component:LoginComponent
+},
+{
+  path:'registerLink',component:RegisterComponent
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
