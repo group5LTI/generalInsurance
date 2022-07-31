80941a50be17f5e041016b2acc5ed558d4729e1a
TanviDeore <tanvi.deore1124@gmail.com>
2022-08-01 00:01:56 +0530

    Merging code

--- insuranceregi/src/app/admin-nav/admin-nav.component.ts
+++ insuranceregi/src/app/admin-nav/admin-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RegisterService } from '../register.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-admin-nav',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-nav.component.css']
 })
 export class AdminNavComponent implements OnInit {
-
-  constructor() { }
+user:User = new User();
+  constructor(private registerService:RegisterService,private router:Router) { }
 
   ngOnInit(): void {
+    this.user=JSON.parse(sessionStorage.getItem("userDetails"));
   }
+  checkLUser(){
+    this.router.navigate(['/homeLink']);
+  }
+  checkHUser(){
+    if(this.user.userName=="Admin"){
+      this.router.navigate(['/adminLink'])
+    }
+    else{
+      this.router.navigate(['/dbLink']);
+    }
+
+  }
+
 
 }
